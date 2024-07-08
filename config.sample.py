305664a64b55d89d70db9c08d6ae05bfec2bea4d
watchful5406 <seantyler525@gmail.com>
2024-07-08 03:07:18 -0700

    update config

--- config.sample.py
+++ config.sample.py
@@ -1,3 +1,6 @@
 webhook_url = "changeme" #change the webhook url before running
 event_report_checks = True #allows the webhook to print NRC event reports
 accepted_plants = ["Columbia Generating Station"] #leave blank to do all
+
+#All Westinghouse PWRs
+#accepted_plants = ["Beaver Valley 1","Beaver Valley 2","Braidwood 1","Braidwood 2","Byron 1","Byron 2","Callaway","Catawba 1","Catawba 2","Comanche Peak 1", "Comanche Peak 2","Diablo Canyon 1","Diablo Canyon 2", "D.C. Cook 1", "D.C. Cook 2","Ginna","Robinson","Farley 1","Farley 2","McGuire 1","McGuire 2","Millstone 2","Millstone 3","North Anna 1", "North Anna 2","Point Beach 1", "Point Beach 2","Prairie Island 1", "Prairie Island 2", "Salem 1", "Salem 2","Seabrook 1","Sequoyah 1","Sequoyah 2","Harris 1","South Texas 1","South Texas 2","Surry 1","Surry 2","Turkey Point 3","Turkey Point 4","Summer","Vogtle 1","Vogtle 2", "Vogtle 3","Vogtle 4","Watts Bar 1", "Watts Bar 2","Wolf Creek 1"]

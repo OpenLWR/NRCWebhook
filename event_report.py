02595b6060fb33038897db5b3d10b4632d2aa84d
watchful5406 <seantyler525@gmail.com>
2024-09-14 14:51:21 -0700

    Add a try loop in case something is broken on NRC's side

--- event_report.py
+++ event_report.py
@@ -64,187 +64,189 @@ def run():
         current_date = (datetime.today()).strftime('%-m/%-d/%Y')
         last_date = (datetime.today() - timedelta(days = 1)).strftime('%-m/%-d/%Y')
 
-    request = requests.get("https://www.nrc.gov/reading-rm/doc-collections/event-status/event/en.html")
+    try:
+        request = requests.get("https://www.nrc.gov/reading-rm/doc-collections/event-status/event/en.html")
 
-    soup = BeautifulSoup(request.content, 'html.parser') 
+        soup = BeautifulSoup(request.content, 'html.parser') 
 
- 
-    #search for everything that has <b>
-    text = soup.find_all(name = "b")
+    
+        #search for everything that has <b>
+        text = soup.find_all(name = "b")
 
-    all_text = []
+        all_text = []
 
-    plants = []
+        plants = []
 
-    for section in text:
-        extracted_text = section.find_next_sibling(string= True)
+        for section in text:
+            extracted_text = section.find_next_sibling(string= True)
 
-        #if the text is Event Text, find the next sibling, then find all descendants that are strings
-        if "Event Text" in section.string:
-            parent = section.find_next_sibling(class_="border")
+            #if the text is Event Text, find the next sibling, then find all descendants that are strings
+            if "Event Text" in section.string:
+                parent = section.find_next_sibling(class_="border")
 
-            parent = parent.find_all(string = True)
-            extracted_text = ""
-            
-            
-            for p in parent:
-                extracted_text = extracted_text+p 
+                parent = parent.find_all(string = True)
+                extracted_text = ""
+                
+                
+                for p in parent:
+                    extracted_text = extracted_text+p 
 
-        if "Person" in section.string:
-            other_text = section.find_next_siblings(string=True)
+            if "Person" in section.string:
+                other_text = section.find_next_siblings(string=True)
 
-            extracted_text = ""
-            for p in other_text:
-                extracted_text = extracted_text+p
+                extracted_text = ""
+                for p in other_text:
+                    extracted_text = extracted_text+p
 
-        #clean it up
-        if not "Event Text" in section.string:
-            extracted_text = extracted_text.replace("\n","")
+            #clean it up
+            if not "Event Text" in section.string:
+                extracted_text = extracted_text.replace("\n","")
 
-        extracted_text = extracted_text.replace("\r","")
-        extracted_text = extracted_text.replace("\t","")
+            extracted_text = extracted_text.replace("\r","")
+            extracted_text = extracted_text.replace("\t","")
 
-        #special formatting for 10 CFR
-        if "Emergency Class" in section.string:
-            other_text = section.find_next_siblings(string=True)
+            #special formatting for 10 CFR
+            if "Emergency Class" in section.string:
+                other_text = section.find_next_siblings(string=True)
 
-            extracted_text = ""
-            for p in other_text:
-                p = p.replace("\n","")
-                p = p.replace("\r","")
-                p = p.replace("\t","")
-                extracted_text = extracted_text+"\n"+p
+                extracted_text = ""
+                for p in other_text:
+                    p = p.replace("\n","")
+                    p = p.replace("\r","")
+                    p = p.replace("\t","")
+                    extracted_text = extracted_text+"\n"+p
 
-        #if we start a new report
-        if "Facility" in section.string or "Rep Org" in section.string:
-            all_text = []
-            plants.append(all_text)
+            #if we start a new report
+            if "Facility" in section.string or "Rep Org" in section.string:
+                all_text = []
+                plants.append(all_text)
 
-        #this is a power reactor, we can grab the table
-        if "Unit" in section.string:
-            rx_table = section.findParent()
-            rx_table = rx_table.findParent()
-            rx_table = rx_table.find_next_sibling()
-            rx_table = rx_table.find_all(string = True)
-            #remove unnecessary stuff
-            while "\n" in rx_table:
-                rx_table.remove("\n")
+            #this is a power reactor, we can grab the table
+            if "Unit" in section.string:
+                rx_table = section.findParent()
+                rx_table = rx_table.findParent()
+                rx_table = rx_table.find_next_sibling()
+                rx_table = rx_table.find_all(string = True)
+                #remove unnecessary stuff
+                while "\n" in rx_table:
+                    rx_table.remove("\n")
 
-            unit = []
-            text_list = []
-            start = False
+                unit = []
+                text_list = []
+                start = False
 
-            for t in rx_table:
-                if t == "1":
-                    start = True
+                for t in rx_table:
+                    if t == "1":
+                        start = True
 
-                if t in ["2","3"]:
-                    if rx_table[rx_table.index(t)+1] in ["Y","N","y","n"]:
-                        unit.append(text_list)
-                        text_list = []
+                    if t in ["2","3"]:
+                        if rx_table[rx_table.index(t)+1] in ["Y","N","y","n"]:
+                            unit.append(text_list)
+                            text_list = []
 
-                if start:
-                    text_list.append(t)
+                    if start:
+                        text_list.append(t)
 
-            if text_list != []:
-                unit.append(text_list)
+                if text_list != []:
+                    unit.append(text_list)
 
-            all_text.append(unit)
+                all_text.append(unit)
+                    
+
                 
 
+            #make it look nicer
+            if ":" in section.string:
+                all_text.append(section.string+extracted_text)
+            else:
+                all_text.append(section.string+":"+extracted_text)
+
+        for plant in plants:
+            is_power_reactor = False
+            for text in plant:
+                if "Unit:" in text:
+                    is_power_reactor = True
+
+            #we dont care about non power reactors
+            if is_power_reactor == False:
+                plants.pop(plants.index(plant))
+
+        #run it twice for shiggles
+        for plant in plants:
+            is_power_reactor = False
+            for text in plant:
+                if "Unit:" in text:
+                    is_power_reactor = True
+
+            #we dont care about non power reactors
+            if is_power_reactor == False:
+                plants.pop(plants.index(plant))
+
+        #maybe a third times a charm?
+        for plant in plants:
+            is_power_reactor = False
+            for text in plant:
+                if "Unit:" in text:
+                    is_power_reactor = True
+
+            #we dont care about non power reactors
+            if is_power_reactor == False:
+                plants.pop(plants.index(plant))
+
+        #why did that work
+
+        for plant in plants:
+
+            embed = make_embed("Event Report",0x00000,"now")
+
+
+            plant_info = ""
+            emergency_class = ""
+            notification_info = ""
+            event_text = ""
+            unit_info = ""
+
+            for text in plant:
+                #TODO: good formatting
+                if "Facility:" in text:
+                    plant_info = plant_info+text+"\n"
+
+                if "Unit:" in text:
+                    plant_info = plant_info+text+"\n"
+
+                if "RX Type:" in text:
+                    plant_info = plant_info+text
+
+                if "Emergency Class:" in text:
+                    emergency_class = emergency_class+text
+
+                if "Event Date:" in text:
+                    notification_info = notification_info+text+"\n"
+
+                if "Event Time:" in text:
+                    notification_info = notification_info+text+"\n"
+
+                if "Last Update Date:" in text:
+                    notification_info = notification_info+text
+
+                if "Event Text" in text:
+                    event_text = (text.replace("Event Text:",""))
+
+                if type(text) == type([]):
+                    unit_info = "Unit|Code|Crit|I PWR|I Mode|C PWR|C Mode\n"
+                    for u in text:
+                        for t in u:
+                            unit_info = unit_info + " " + t
+
+                        unit_info = unit_info + "\n"
+
+            embed.add_field(name = "Plant Info", value = discord_funky(plant_info))
+            embed.add_field(name = "Notification Info", value = discord_funky(notification_info))
+            embed.add_field(name = "Emergency Class", value = discord_funky(emergency_class), inline = False)
+            embed.add_field(name = "Unit Info", value = discord_funky(unit_info), inline = False)
             
 
-        #make it look nicer
-        if ":" in section.string:
-            all_text.append(section.string+extracted_text)
-        else:
-            all_text.append(section.string+":"+extracted_text)
-
-    for plant in plants:
-        is_power_reactor = False
-        for text in plant:
-            if "Unit:" in text:
-                is_power_reactor = True
-
-        #we dont care about non power reactors
-        if is_power_reactor == False:
-            plants.pop(plants.index(plant))
-
-    #run it twice for shiggles
-    for plant in plants:
-        is_power_reactor = False
-        for text in plant:
-            if "Unit:" in text:
-                is_power_reactor = True
-
-        #we dont care about non power reactors
-        if is_power_reactor == False:
-            plants.pop(plants.index(plant))
-
-    #maybe a third times a charm?
-    for plant in plants:
-        is_power_reactor = False
-        for text in plant:
-            if "Unit:" in text:
-                is_power_reactor = True
-
-        #we dont care about non power reactors
-        if is_power_reactor == False:
-            plants.pop(plants.index(plant))
-
-    #why did that work
-
-    for plant in plants:
-
-        embed = make_embed("Event Report",0x00000,"now")
-
-
-        plant_info = ""
-        emergency_class = ""
-        notification_info = ""
-        event_text = ""
-        unit_info = ""
-
-        for text in plant:
-            #TODO: good formatting
-            if "Facility:" in text:
-                plant_info = plant_info+text+"\n"
-
-            if "Unit:" in text:
-                plant_info = plant_info+text+"\n"
-
-            if "RX Type:" in text:
-                plant_info = plant_info+text
-
-            if "Emergency Class:" in text:
-                emergency_class = emergency_class+text
-
-            if "Event Date:" in text:
-                notification_info = notification_info+text+"\n"
-
-            if "Event Time:" in text:
-                notification_info = notification_info+text+"\n"
-
-            if "Last Update Date:" in text:
-                notification_info = notification_info+text
-
-            if "Event Text" in text:
-                event_text = (text.replace("Event Text:",""))
-
-            if type(text) == type([]):
-                unit_info = "Unit|Code|Crit|I PWR|I Mode|C PWR|C Mode\n"
-                for u in text:
-                    for t in u:
-                        unit_info = unit_info + " " + t
-
-                    unit_info = unit_info + "\n"
-
-        embed.add_field(name = "Plant Info", value = discord_funky(plant_info))
-        embed.add_field(name = "Notification Info", value = discord_funky(notification_info))
-        embed.add_field(name = "Emergency Class", value = discord_funky(emergency_class), inline = False)
-        embed.add_field(name = "Unit Info", value = discord_funky(unit_info), inline = False)
-        
-
-        send_within_char_limit(embed,event_text)
+            send_within_char_limit(embed,event_text)
+    except:
 
         time.sleep(10)
--- main.py
+++ main.py
@@ -29,67 +29,69 @@ while True:
     else:
         current_date = (datetime.today()).strftime('%-m/%-d/%Y')
         last_date = (datetime.today() - timedelta(days = 1)).strftime('%-m/%-d/%Y')
+    try:
+        request = requests.get("https://www.nrc.gov/reading-rm/doc-collections/event-status/reactor-status/powerreactorstatusforlast365days.txt")
 
-    request = requests.get("https://www.nrc.gov/reading-rm/doc-collections/event-status/reactor-status/powerreactorstatusforlast365days.txt")
+        lines = request.text.split("\r\n")
+        lines.pop(0)
 
-    lines = request.text.split("\r\n")
-    lines.pop(0)
+        if previous_date != current_date and current_date in lines[0]:
 
-    if previous_date != current_date and current_date in lines[0]:
+            previous_date = current_date
 
-        previous_date = current_date
+            formatted_lines = []
 
-        formatted_lines = []
+            last_powers = []
 
-        last_powers = []
+            for line in lines:
+                if line == " ": 
+                    continue
 
-        for line in lines:
-            if line == " ": 
-                continue
+                line_content = line.split("|")
+                date = line_content[0]
+                plant_name = line_content[1]
+                power = line_content[2]
 
-            line_content = line.split("|")
-            date = line_content[0]
-            plant_name = line_content[1]
-            power = line_content[2]
+                date = date.split(" ")[0]
 
-            date = date.split(" ")[0]
+                if not plant_name in config.accepted_plants and config.accepted_plants != []:
+                    continue
 
-            if not plant_name in config.accepted_plants and config.accepted_plants != []:
-                continue
-
-            if date == current_date:
-                formatted_lines.append("%s : %s" % (plant_name,power)) 
-            elif date == last_date:
-                last_powers.append(power)
-            else:
-                break
-
-
-
-
-        big_string = ""
-
-        for line in formatted_lines:
-
-            line_index = formatted_lines.index(line)
-            last_power = last_powers[line_index]
-            current_power = line.split(" : ")[1]
-
-            if last_power != current_power:
-                line = "**__"+line+"__**"
-
-            if big_string == "":
-                big_string = line
-            else:
-                big_string = big_string+"\r\n"+line
+                if date == current_date:
+                    formatted_lines.append("%s : %s" % (plant_name,power)) 
+                elif date == last_date:
+                    last_powers.append(power)
+                else:
+                    break
 
 
 
 
-        embed = make_embed(big_string,0x00000,current_date)
-        hook.send(embed=embed)
+            big_string = ""
 
-    if config.event_report_checks:
-        event_report.run()
+            for line in formatted_lines:
 
-    time.sleep(3600)
+                line_index = formatted_lines.index(line)
+                last_power = last_powers[line_index]
+                current_power = line.split(" : ")[1]
+
+                if last_power != current_power:
+                    line = "**__"+line+"__**"
+
+                if big_string == "":
+                    big_string = line
+                else:
+                    big_string = big_string+"\r\n"+line
+
+
+
+
+            embed = make_embed(big_string,0x00000,current_date)
+            hook.send(embed=embed)
+
+        if config.event_report_checks:
+            event_report.run()
+
+        time.sleep(3600)
+    except:
+        time.sleep(600) #Maybe their servers are cooked?
